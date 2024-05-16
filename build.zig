4312bb44b7197b2f310793e4312990e26b0cb469
Sam Atman <atmanistan@gmail.com>
2024-05-16 10:41:12 -0400

    Rename root to runelib

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) void {
         .name = "runeset",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/runelib.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -67,7 +67,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/runelib.zig"),
         .target = target,
         .optimize = optimize,
     });
